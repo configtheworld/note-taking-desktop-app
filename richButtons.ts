3a16b07692353f260e7af5a5900e07e487d00304
configtheworld <erkinture@gmail.com>
2022-04-08 01:20:02 +0300

    next tasks noted

---
+++ richButtons.ts
@@ -0,0 +1,15 @@
+// update context like this https://stackoverflow.com/questions/11076975/how-to-insert-text-into-the-textarea-at-the-current-cursor-position
+
+// Bold
+
+// Italic
+
+// Code
+
+// List
+
+// Quote
+
+// Image
+
+// Link
